064219afad6a947c8a31d2fd866688a8720f3d92
cyberpope <md@pope.land>
2019-05-26 11:38:20 +0100

    Second type of links detection

--- entura_del_embed.py
+++ entura_del_embed.py
@@ -3,22 +3,36 @@ import pyperclip
 import re
 
 # Regex
-links_regex = re.compile(r'(https:/|http:/)(/.*?/)(embed/)?(.*)')
-embed_regex = re.compile(r'(https:/|http:/)(/.*?/)(embed/)(.*)')
+embed_regex = re.compile(r'(https:/|http:/)(/.*?/)(embed/)?(.*)')
+embed_check_regex = re.compile(r'(https:/|http:/)(/.*?/)(embed/)(.*)')
+uploadedTo_ref_regex = re.compile(r'(https:/|http:/)(/.*?)(\?ref=\d*)?')
+uploadedTo_ref_check_regex = re.compile(r'(https:/|http:/)(/.*?)(\?ref=\d*)')
+site_name_regex = re.compile(r'(https://|http://)(www.)?(.*?)/')
 
 # Clipbord matches check + link clearing
 text = str(pyperclip.paste())
 
 matches = []
-for groups in links_regex.findall(text):
-#    print(groups)
-    clear_link = ''.join([groups[0], groups[1], groups[3]])
-#    print(clear_link)
-    matches.append(clear_link)
-if len(matches) > 0:
-    pyperclip.copy('\n'.join(matches))
-    print('Done master, total links count: %s ' % len(matches))
-#   print('\n'.join(matches))
+
+# site_name = site_name_regex.findall(text)
+for groups in site_name_regex.findall(text):
+    site_name = ''.join([groups[2]])
+
+if embed_check_regex.findall(text):
+    for groups in embed_regex.findall(text):
+        clear_link = ''.join([groups[0], groups[1], groups[3]])
+        matches.append(clear_link)
+    if len(matches) > 0:
+        pyperclip.copy('\n'.join(matches))
+        print('Done, embed links found for %s, total count: %s ' % (site_name, len(matches)))
+elif uploadedTo_ref_check_regex.findall(text):
+    for groups in uploadedTo_ref_regex.findall(text):
+        clear_link = ''.join([groups[0], groups[1]])
+        print(clear_link)
+        matches.append(clear_link)
+    if len(matches) > 0:
+        pyperclip.copy('\n'.join(matches))
+        print('Done, uploadedTo ref links found for %s, total count: %s ' % (site_name, len(matches)))
 else:
     print('No links found')
 
